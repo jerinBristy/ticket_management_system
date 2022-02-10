78d3f553ebbc5334feca03bc15ff2460f276eb6d
jerinBristy <ijerin1112@gmail.com>
2022-02-10 17:39:24 +0600

    implemented Model

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <h1>Login!</h1>
+    <form method="POST" action="/login">
+        <input type="text" class="text">
+        <input type="text" class="password">
+        <button class="submit">Login</button>
+    </form>
+</x-layout>
