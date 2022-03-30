4e7b613b01f4974e483b96e736d288ea208fa3dd
jerinBristy <ijerin1112@gmail.com>
2022-03-30 10:56:50 +0600

    fixed create page

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -7,43 +7,21 @@
                 <h3>Regular Seat Price: {{$routeSeatTypes[0]->pivot->price}}</h3>
                 <h3>Premium Seat Price: {{$routeSeatTypes[1]->pivot->price}}</h3>
                 <div class="seatBox">
-                    <span hidden>{{$letterCount=-1}}</span>
                     @if(count($premiumSeats)!=0)
-                        <span hidden>{{$letterCount+=1}}</span>
                         <div class="premiumBox">
-                            {{--                    <x-seats :letters="$letters" :seats="$premiumSeats" className="premiumSeats" :letterCount/>--}}
-                            <span hidden>{{$count=0}}</span>
-                            @for($i=0;$i<count($letters)-1;)
-                                @foreach($premiumSeats as $seat)
-                                    @if($count>2)
-                                        <span hidden>{{$count=0}}</span><span hidden>{{$i++}}</span>
-                                    @endif
-                                    <input id="seat{{$letters[$i]}}{{$count}}" value="{{$seat->id}}" name="seats[]"
-                                           type="checkbox" class="cbox" style="display: none; position: absolute"/>
-                                        <label for="seat{{$letters[$i]}}{{$count}}" class="premiumSeats seatName">
-                                            {{$letters[$i]}}{{$count}}
-                                        </label>
-                                    <span hidden>{{$count++}}</span>
-                                    <span hidden>{{$letterCount=$i}}</span>
-                                    <br>
-                                @endforeach
-                                @break
-                            @endfor
+                            <x-seats :seats="$premiumSeats" className="premiumSeats seatName" row="2"/>
                         </div>
                     @endif
                     <div class="regularBox">
-
-                        <x-seats :letters="$letters" :seats="$regularSeats" className="regularSeats seatName" :letterCount="$letterCount"/>
+                        <x-seats :seats="$regularSeats" className="regularSeats seatName" row="3"/>
                     </div>
-
                 </div>
             </div>
             <div class="passenger-info">
                 <h1>Passenger Information</h1>
-                    <x-form.input name="name"/>
-                    <x-form.input name="phone"/>
-                    <button type="submit" class="btn">Book</button>
-
+                <x-form.input name="name"/>
+                <x-form.input name="phone"/>
+                <button type="submit" class="btn">Book</button>
             </div>
         </form>
     </div>
--- resources/views/components/seats.blade.php
+++ resources/views/components/seats.blade.php
@@ -1,21 +1,16 @@
-@props(['letters','seats','className','letterCount'])
+@props(['seats','className', 'row'])
 
 <span hidden>{{$count=0}}</span>
-@for($i=$letterCount+1;$i<count($letters)-1;)
-    @foreach($seats as $seat)
-
-        @if($count>3)
-            <span hidden>{{$count=0}}</span><span hidden>{{$i++}}</span>
-        @endif
-        <input id="seat{{$letters[$i]}}{{$count}}" name="seats[]"
-               type="checkbox" class="cbox" value="{{$seat->id}}" style="display: none; position: absolute"/>
-        <label for="seat{{$letters[$i]}}{{$count}}" class="{{$className}}">
-            {{$letters[$i]}}{{$count}}
-        </label>
-        <span hidden>{{$count++}}</span>
-        <br>
-    @endforeach
-    @break
-@endfor
-
+@foreach($seats as $seat)
+    @if($count>$row)
+        <span hidden>{{$count=0}}</span>
+    @endif
+    <input id="seat{{$seat->name}}" name="seats[]"
+           type="checkbox" class="cbox" value="{{$seat->id}}" style="display: none; position: absolute"/>
+    <label for="seat{{$seat->name}}" class="{{$className}}">
+        {{$seat->name}}
+    </label>
+    <span hidden>{{$count++}}</span>
+    <br>
+@endforeach
 
